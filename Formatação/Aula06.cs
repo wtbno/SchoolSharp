8552df2738a9241a663195a91beafb34a68e3779
wtbno <bruno_links@hotmail.com>
2023-01-23 22:00:41 -0300

    formatação

---
+++ Formatação/Aula06.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula06
+{
+    static void Main(string[] args)
+    {
+        int n1, n2, n3;
+        n1 = 15;
+        n2 = 38;
+        n3 = 67;
+
+        System.Console.WriteLine("\nn1={0}, \nn2={1}, \nn3={2}", n1, n2, n3);
+    }
+}
