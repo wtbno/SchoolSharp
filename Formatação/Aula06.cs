3ccc1dd641f5f79ead0e85c63b87d3a6ed20c072
wtbno <bruno_links@hotmail.com>
2023-01-24 20:48:32 -0300

    aula 06

--- Formatação/Aula06.cs
+++ Formatação/Aula06.cs
@@ -2,13 +2,19 @@ using System;
 
 class Aula06
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        int n1, n2, n3;
-        n1 = 15;
-        n2 = 38;
-        n3 = 67;
+        double valorCompra = 5.50;
+        double valorVenda;
+        double lucro = 0.1;
+        string produto = "Pastel";
 
-        System.Console.WriteLine("\nn1={0}, \nn2={1}, \nn3={2}", n1, n2, n3);
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        System.Console.WriteLine("Produto.......:{0.15}", produto);
+        System.Console.WriteLine("Valor de compra....:{0.15:c}", valorCompra);
+        System.Console.WriteLine("Lucro.....:{0.15:p}", lucro);
+
+
     }
 }
