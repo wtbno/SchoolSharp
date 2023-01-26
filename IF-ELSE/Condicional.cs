de6b337cf0b296783060b8803b9616873ba9bdec
wtbno <bruno_links@hotmail.com>
2023-01-26 14:03:54 -0300

    Feat. if | else

---
+++ IF-ELSE/Condicional.cs
@@ -0,0 +1,16 @@
+using System;
+
+static void Main(string[] args)
+{
+    int a = 15;
+    int b = 97;
+    if (a + b > 100)
+    {
+        System.Console.WriteLine("O valor é superior a 100");
+    }
+
+    else
+    {
+        System.Console.WriteLine("O valor é inferior a 100");
+    }
+}
